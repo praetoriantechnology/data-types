abf9f999c04d538972890fb5e6b37b7af1b247ca
Davor Minchorov <davorminchorov@gmail.com>
2022-08-30 10:09:21 +0200

    Add additional methods to the Integers class

--- src/Integers.php
+++ src/Integers.php
@@ -30,4 +30,75 @@ class Integers
     {
         return ($num >= $from && $num <= $to);
     }
+
+    /**
+     * @param string $hex
+     * @return BcNumber
+     */
+    public static function Unpack(string $hex): BcNumber
+    {
+        if (str_starts_with($hex, '0x')) {
+            $hex = substr($hex, 2);
+        }
+
+        if (!$hex) {
+            return new BcNumber(0);
+        }
+
+        $hex = self::HexitPads($hex);
+        return new BcNumber(gmp_strval(gmp_init($hex, 16), 10));
+    }
+
+    /**
+     * @param  int|string  $dec
+     * @return string
+     */
+    public static function Pack_UInt_BE(int|string $dec): string
+    {
+        $dec = self::checkValidInt($dec);
+
+        return self::HexitPads(bin2hex(gmp_export(gmp_init($dec, 10), 1, GMP_MSW_FIRST | GMP_NATIVE_ENDIAN)));
+    }
+
+    /**
+     * @param $dec
+     * @return string
+     */
+    public static function Pack_UInt_LE($dec): string
+    {
+        $dec = self::checkValidInt($dec);
+        return self::HexitPads(bin2hex(gmp_export(gmp_init($dec, 10), 1, GMP_LSW_FIRST | GMP_NATIVE_ENDIAN)));
+    }
+
+    /**
+     * @param string $hex
+     * @return string
+     */
+    public static function HexitPads(string $hex): string
+    {
+        if (strlen($hex) % 2 !== 0) {
+            $hex = "0" . $hex;
+        }
+
+        return $hex;
+    }
+
+    /**
+     * @param  int|string|BcNumber  $dec
+     * @return int|string
+     */
+    public static function checkValidInt(BcNumber|int|string $dec): int|string
+    {
+        if ($dec instanceof BcNumber && $dec->isInteger()) {
+            $dec = $dec->value();
+        }
+
+        if (!is_int($dec)) {
+            if (!is_string($dec) || !preg_match('/^-?(0|[1-9]+[0-9]*)$/', $dec)) {
+                throw new \InvalidArgumentException('Argument must be a valid INT');
+            }
+        }
+
+        return $dec;
+    }
 }
